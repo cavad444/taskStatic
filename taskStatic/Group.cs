8be45658e80f7c145c870d0374ed1203db8b0e26
cavad444 <118839594+cavad444@users.noreply.github.com>
2022-12-29 15:17:24 +0400

    Update Group.cs

--- taskStatic/Group.cs
+++ taskStatic/Group.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +10,13 @@ namespace taskStatic
     {
         private static int _id;
         private string _name;
-        public string[] Students = new string[_id];
+        public Student[] Students = new Student[0];
 
         public int Id { get { return _id; } set { _id = value; } }
         public string Name { get { return _name; } set { _name = value; } }
 
 
-       
+
 
     }
 }
